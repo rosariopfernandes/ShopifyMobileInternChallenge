9458914bad5ba7eea2ef1e1185357e03d46a8c9a
Rosário Pereira Fernandes <rosariofernandes51@gmail.com>
2018-09-19 16:58:09 +0200

    refactor: Use the Navigation Arch Component to navigate between screens

--- app/src/main/java/com/myshopify/shopicruit/shopifyinternchallenge/productslist/ProductsFragment.kt
+++ app/src/main/java/com/myshopify/shopicruit/shopifyinternchallenge/productslist/ProductsFragment.kt
@@ -1,35 +1,37 @@
 package com.myshopify.shopicruit.shopifyinternchallenge.productslist
 
+
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import com.myshopify.shopicruit.shopifyinternchallenge.R
-import com.myshopify.shopicruit.shopifyinternchallenge.tagslist.TagsListActivity
-import kotlinx.android.synthetic.main.activity_products.*
 import kotlinx.android.synthetic.main.content_simple_list.*
 
-class ProductsActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_products)
-        setSupportActionBar(toolbar)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+class ProductsFragment : Fragment() {
 
-        val productTag = intent.extras.getString(TagsListActivity.PRODUCT_TAG_KEY)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val fragmentView = inflater.inflate(R.layout.content_simple_list, container, false)
+
+        val productTag = ProductsFragmentArgs.fromBundle(arguments).productTag
+
         val viewModel = ViewModelProviders.of(this).get(ProductsViewModel::class.java)
         viewModel.getProducts(productTag).observe(this, Observer { products ->
             products?.let {
-                recyclerView.layoutManager = LinearLayoutManager(this)
+                recyclerView.layoutManager = LinearLayoutManager(context)
                 recyclerView.adapter = ProductsAdapter(it)
                 recyclerView.visibility = View.VISIBLE
                 ivEmptyState.visibility = View.GONE
             }
         })
 
+        return fragmentView
     }
 
 }
---
+++ app/src/main/java/com/myshopify/shopicruit/shopifyinternchallenge/tagslist/TagsFragment.kt
@@ -0,0 +1,54 @@
+package com.myshopify.shopicruit.shopifyinternchallenge.tagslist
+
+
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.myshopify.shopicruit.shopifyinternchallenge.R
+import kotlinx.android.synthetic.main.content_simple_list.*
+import mz.co.kidzkare.vaccines.util.RecyclerItemClickListener
+
+
+class TagsFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val fragmentView = inflater.inflate(R.layout.content_simple_list, container, false)
+
+        val viewModel = ViewModelProviders.of(this).get(TagsViewModel::class.java)
+        viewModel.getTags().observe(this, Observer { tags ->
+            tags?.let {
+                recyclerView.layoutManager = LinearLayoutManager(context)
+                recyclerView.adapter = TagsAdapter(it)
+                recyclerView.visibility = View.VISIBLE
+                ivEmptyState.visibility = View.GONE
+
+                recyclerView.addOnItemTouchListener(
+                        RecyclerItemClickListener(
+                                recyclerView,
+                                object : RecyclerItemClickListener.OnItemClickListener {
+                                    override fun onItemClick(view: View, position: Int) {
+                                        val selectedTag = it[position]
+                                        val action =
+                                                TagsFragmentDirections.actionViewProducts()
+                                        action.setProductTag(selectedTag)
+                                        findNavController().navigate(action)
+                                    }
+
+                                    override fun onLongItemClick(view: View, position: Int) {
+
+                                    }
+                                }))
+            }
+        })
+
+        return fragmentView
+    }
+
+}
